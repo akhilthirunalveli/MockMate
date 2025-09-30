44d63abaa0667973e1425b04a4aad0bde9fdea38
akhilthirunalveli <thirunalveliakhil@gmail.com>
2025-09-30 15:56:59 +0530

    Improved AuthRoutes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,12 +4,11 @@ const { protect } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-// Auth Routes
-router.post("/register", registerUser);   // Register User
-router.post("/login", loginUser);         // Login User
-router.get("/profile", protect, getUserProfile);  // Get User Profile
-router.put("/resume-link", protect, updateResumeLink);  // Update Resume Link
-router.get("/users", getAllUsers);       // Get All Users
-router.delete("/users/:id", deleteUser); // Delete User
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/profile", protect, getUserProfile);
+router.put("/resume-link", protect, updateResumeLink);
+router.get("/users", getAllUsers);
+router.delete("/users/:id", deleteUser);
 
 module.exports = router;
